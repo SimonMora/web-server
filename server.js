2dd87ffbf40325f3fa3fe476b7691cd3cc9ec921
simon.a.mora.suarez <simon.a.mora.suarez@accenture.com>
2019-12-09 16:06:03 -0300

    cambios en el server, se añaden bootstrap a las paginas

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
-app.listen(8888);
+app.use(express.static(__dirname+'/public'));
+
+//express handlebars engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//helpers
+hbs.registerHelper('getYear', function(){
+    return new Date().getFullYear(); 
+});
+
+hbs.registerHelper('capitalize', (text)=>{
+    
+    let palabras = text.split(' ');
+    palabras.forEach((element,index) => {
+        palabras[index] = element[0].toUpperCase() + element.slice(1).toLowerCase();
+    });
+
+    return palabras.join(' ');
+});
+
+
+app.listen(8888,()=>{
+    console.log('Servidor de alta en el puerto: '+ 8888);
+});
+
 app.get('/',(req, res)=>{
-    let data = {
-        name: "Dolores White",
-        edad: 23,
-        url: req.url
-    }
-    res.send(data);
+    
+    res.render('home', {
+        name : 'saimON tHE Best',
+    });
+    
+});
+app.get('/about',(req, res)=>{
+
+    res.render('about',{
+        name : 'saimON tHE Best',
+    });
+    
 });
